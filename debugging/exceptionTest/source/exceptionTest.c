b51121261c9e7aade34b9893e6f16f4d35d68aa7
Dave Murphy <wntrmute@users.sourceforge.net>
2006-06-18 21:33:48 +0000

    added exception test example

---
+++ debugging/exceptionTest/source/exceptionTest.c
@@ -0,0 +1,25 @@
+#include <nds.h>
+
+// The default exception handler displays the exception type - data abort or undefined instruction
+// you can relate the exception to your code using arm-eabi-addr2line -e <elf file> <address>
+// assuming you built with debug info this will display a source file and a line number
+// The address of the instruction is shown as pc, beside the address which faulted
+// the rest of the screen is a dump of the registers.
+
+//---------------------------------------------------------------------------------
+int main(void) {
+//---------------------------------------------------------------------------------
+
+	// install the default exception handler
+	setExceptionHandler(defaultExceptionHandler) ;
+
+	irqInit();
+	irqEnable(IRQ_VBLANK);
+
+	// generate an exception
+	*(u32*)250 = 100;
+
+	while(1) swiWaitForVBlank();
+
+	return 0;
+}
