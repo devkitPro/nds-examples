b75c1fce805fbc47ceb2f54b531e7ca8507636d0
Jason Rogers <dovoto@users.sourceforge.net>
2007-01-09 06:23:05 +0000

    Fixed logging header

--- ds_motion/source/main.c
+++ ds_motion/source/main.c
@@ -1,23 +1,12 @@
 /*---------------------------------------------------------------------------------
-	$Id: main.c,v 1.1 2007-01-09 06:14:40 dovoto Exp $
+	$Id: main.c,v 1.2 2007-01-09 06:23:05 dovoto Exp $
 
-	Basic Hello World
+	Basic DS Motion example
 
 	$Log: not supported by cvs2svn $
-	Revision 1.4  2005/09/17 23:15:13  wntrmute
-	corrected iprintAt in templates
+	Revision 1.1  2007/01/09 06:14:40  dovoto
+	Added example for ds motion
 	
-	Revision 1.3  2005/09/05 00:32:20  wntrmute
-	removed references to IPC struct
-	replaced with API functions
-	
-	Revision 1.2  2005/08/31 01:24:21  wntrmute
-	updated for new stdio support
-
-	Revision 1.1  2005/08/03 06:29:56  wntrmute
-	added templates
-
-
 ---------------------------------------------------------------------------------*/
 #include <nds.h>
 #include <stdio.h>
