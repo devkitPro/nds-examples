04c462ef4967bdce0feb3f2589382fe7a2e4c7d0
Jason Rogers <dovoto@users.sourceforge.net>
2007-01-10 16:43:18 +0000

    Update motion example to demonstrate new calibration functionality

--- ds_motion/source/main.c
+++ ds_motion/source/main.c
@@ -1,9 +1,12 @@
 /*---------------------------------------------------------------------------------
-	$Id: main.c,v 1.2 2007-01-09 06:23:05 dovoto Exp $
+	$Id: main.c,v 1.3 2007-01-10 16:43:18 dovoto Exp $
 
 	Basic DS Motion example
 
 	$Log: not supported by cvs2svn $
+	Revision 1.2  2007/01/09 06:23:05  dovoto
+	Fixed logging header
+	
 	Revision 1.1  2007/01/09 06:14:40  dovoto
 	Added example for ds motion
 	
@@ -12,6 +15,8 @@
 #include <stdio.h>
 
 #include <nds/arm9/ndsmotion.h>
+
+void Calibrate();
 
 //---------------------------------------------------------------------------------
 int main(void) {
@@ -23,25 +28,42 @@ int main(void) {
 	consoleDemoInit();
 	
 	motion_enable();
-	
+
 	
 	
 	while(1) 
-	{
-			int x = motion_read_x();
-			int y = motion_read_y();
-			int z = motion_read_z();
-			int rot = motion_read_gyro();
-			
-			consoleClear();
-			iprintf("Nds is%s inserted\n", motion_is_inserted() ? "" : " not");
-			iprintf("X: raw %i  miliG %i\n", x, motion_acceleration(x));
-			iprintf("Y: raw %i  miliG %i\n", y, motion_acceleration(y));
-			iprintf("Z: raw %i  miliG %i\n", z, motion_acceleration(z));
-			iprintf("R: raw %i  deg/sec %i\n", rot, motion_rotation(rot));
-  
-			swiWaitForVBlank();
+	{	
+		scanKeys();
+		
+		if(keysDown() & KEY_A) Calibrate();
+		
+		consoleClear();
+		iprintf("Nds is%s inserted\n", motion_is_inserted() ? "" : " not");
+		iprintf("X: raw %i  miliG %i\n", motion_read_x(), motion_acceleration_x());
+		iprintf("Y: raw %i  miliG %i\n", motion_read_y(), motion_acceleration_y());
+		iprintf("Z: raw %i  miliG %i\n", motion_read_z(), motion_acceleration_z());
+		iprintf("R: raw %i  deg/sec %i\n", motion_read_gyro(), motion_rotation());
+		iprintf("Press A to calibrate\n");	
+		swiWaitForVBlank();
 	}
 
 	return 0;
+}
+
+//sets the offset of the x and y and gyro and the 1 G offset of z
+//to callivrate other offsets you would have to instruct the user to orient
+//the DS in various positions
+void Calibrate(){
+
+	iprintf("Set the DS on a flat table and press A\n");
+	
+	while(!keysDown() & KEY_A) scanKeys();
+	
+	//these set the zero points of the accelerometers and gryo
+	motion_set_offs_x();
+	motion_set_offs_y();
+	motion_set_offs_gyro();
+	
+	//this should be set to the acceleration value at 1 z
+	motion_set_sens_z(motion_read_z());
 }
