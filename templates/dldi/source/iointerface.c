38a608a4fb60b42c8e7dc1ad0ca383e6471d11b0
Michael Chisholm <chishm@users.sourceforge.net>
2007-02-15 02:06:17 +0000

    Updated license and comments

--- templates/dldi/source/iointerface.c
+++ templates/dldi/source/iointerface.c
@@ -1,17 +1,14 @@
 /*
-	iointerface.c
+	iointerface.c template
 	
  Copyright (c) 2006 Michael "Chishm" Chisholm
 	
  Redistribution and use in source and binary forms, with or without modification,
  are permitted provided that the following conditions are met:
 
-  1. Redistributions of source code must retain the above copyright notice,
-     this list of conditions and the following disclaimer.
-  2. Redistributions in binary form must reproduce the above copyright notice,
-     this list of conditions and the following disclaimer in the documentation and/or
-     other materials provided with the distribution.
-  3. The name of the author may not be used to endorse or promote products derived
+  1. All derivative works must be clearly marked as such. Derivatives of this file 
+	 must have the author of the derivative indicated within the source.  
+  2. The name of the author may not be used to endorse or promote products derived
      from this software without specific prior written permission.
 
  THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED
@@ -67,7 +64,7 @@ bool isInserted (void) {
 /*-----------------------------------------------------------------
 clearStatus
 Reset the card, clearing any status errors
-return  true if the card is idle and ready
+return true if the card is idle and ready
 -----------------------------------------------------------------*/
 bool clearStatus (void) {
 	return false;
@@ -77,7 +74,8 @@ bool clearStatus (void) {
 /*-----------------------------------------------------------------
 readSectors
 Read "numSectors" 512-byte sized sectors from the card into "buffer", 
-starting at "sector".
+starting at "sector". 
+The buffer may be unaligned, and the driver must deal with this correctly.
 return true if it was successful, false if it failed for any reason
 -----------------------------------------------------------------*/
 bool readSectors (u32 sector, u32 numSectors, void* buffer) {
@@ -90,6 +88,7 @@ bool readSectors (u32 sector, u32 numSectors, void* buffer) {
 writeSectors
 Write "numSectors" 512-byte sized sectors from "buffer" to the card, 
 starting at "sector".
+The buffer may be unaligned, and the driver must deal with this correctly.
 return true if it was successful, false if it failed for any reason
 -----------------------------------------------------------------*/
 bool writeSectors (u32 sector, u32 numSectors, void* buffer) {
@@ -99,6 +98,9 @@ bool writeSectors (u32 sector, u32 numSectors, void* buffer) {
 /*-----------------------------------------------------------------
 shutdown
 shutdown the card, performing any needed cleanup operations
+Don't expect this function to be called before power off, 
+it is merely for disabling the card.
+return true if the card is no longer active
 -----------------------------------------------------------------*/
 bool shutdown(void) {
 	return false;
