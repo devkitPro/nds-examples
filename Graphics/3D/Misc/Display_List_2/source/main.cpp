f585688c46072622aac00690c1501abc23527ada
Gabe Ghearing <gabebear@users.sourceforge.net>
2007-02-22 21:55:03 +0000

    update example for libnds changes and enable antialiasing

--- Graphics/3D/Misc/Display_List_2/source/main.cpp
+++ Graphics/3D/Misc/Display_List_2/source/main.cpp
@@ -5,8 +5,7 @@
 
 
 int main()
-{	
-
+{
 	float rotateX = 0.0;
 	float rotateY = 0.0;
 
@@ -21,57 +20,45 @@ int main()
 
 	// initialize gl
 	glInit();
+	
+	// enable antialiasing
+	glEnable(GL_ANTIALIAS);
+	
+	// setup the rear plane
+	glClearColor(0,0,0,31); // BG must be opaque for AA to work
+	glClearPolyID(63); // BG must have a unique polygon ID for AA to work
+	glClearDepth(0x7FFF);
 
 	//this should work the same as the normal gl call
 	glViewPort(0,0,255,191);
 	
-	glClearColor(0,0,0);
-	glClearDepth(0x7FFF);
+	//any floating point gl call is being converted to fixed prior to being implemented
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(35, 256.0 / 192.0, 0.1, 40);
 	
+	gluLookAt(	0.0, 0.0, 3.5,		//camera possition 
+				0.0, 0.0, 0.0,		//look at
+				0.0, 1.0, 0.0);		//up
 	
+	glLight(0, RGB15(31,31,31) , 0,				  floattov10(-1.0),		 0);
+	glLight(1, RGB15(31,0,31),   0,				  floattov10(1) - 1,			 0);
+	glLight(2, RGB15(0,31,0) ,   floattov10(-1.0), 0,					 0);
+	glLight(3, RGB15(0,0,31) ,   floattov10(1.0) - 1,  0,					 0);
+	
+	//not a real gl function and will likely change
+	glPolyFmt(POLY_ALPHA(31) | POLY_CULL_BACK | POLY_FORMAT_LIGHT0 | POLY_FORMAT_LIGHT1 | 
+			  POLY_FORMAT_LIGHT2 | POLY_FORMAT_LIGHT3 ) ;
 	
 	while(1)		
 	{
-	
-		//any floating point gl call is being converted to fixed prior to being implemented
-		glMatrixMode(GL_PROJECTION);
-		glLoadIdentity();
-		gluPerspective(35, 256.0 / 192.0, 0.1, 40);
-
-		gluLookAt(	0.0, 0.0, 1.0,		//camera possition 
-					0.0, 0.0, 0.0,		//look at
-					0.0, 1.0, 0.0);		//up
-		
-		
-
-		
-		glLight(0, RGB15(31,31,31) , 0,				  floattov10(-1.0),		 0);
-		glLight(1, RGB15(31,0,31),   0,				  floattov10(1) - 1,			 0);
-		glLight(2, RGB15(0,31,0) ,   floattov10(-1.0), 0,					 0);
-		glLight(3, RGB15(0,0,31) ,   floattov10(1.0) - 1,  0,					 0);
-
 		glPushMatrix();
-
-		//move it away from the camera
-		glTranslate3f32(0, 0, floattof32(-1));
 				
 		glRotateX(rotateX);
 		glRotateY(rotateY);
 		
-		glMatrixMode(GL_TEXTURE);
-		glIdentity();
-		
-		glMatrixMode(GL_MODELVIEW);
-
-	
-		//not a real gl function and will likely change
-		glPolyFmt(POLY_ALPHA(31) | POLY_CULL_BACK | POLY_FORMAT_LIGHT0 | POLY_FORMAT_LIGHT1 | 
-													POLY_FORMAT_LIGHT2 | POLY_FORMAT_LIGHT3 ) ;
-		
 		scanKeys();
-		
 		u16 keys = keysHeld();
-		
 		if(!(keys & KEY_UP)) rotateX += 3;
 		if(!(keys & KEY_DOWN)) rotateX -= 3;
 		if(!(keys & KEY_LEFT)) rotateY += 3;
@@ -82,8 +69,6 @@ int main()
 		glPopMatrix(1);
 			
 		glFlush();
-
-		swiWaitForVBlank();
 	}
 
 	return 0;
