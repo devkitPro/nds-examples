833ca165e17d4f0eca5f12e7c92a86c0e9faced3
Dave Murphy <wntrmute@users.sourceforge.net>
2007-10-23 15:49:10 +0000

    update default arm7

--- templates/combined/arm7/source/template.c
+++ templates/combined/arm7/source/template.c
@@ -1,5 +1,31 @@
+/*---------------------------------------------------------------------------------
+
+	default ARM7 core
+
+	Copyright (C) 2005
+		Michael Noland (joat)
+		Jason Rogers (dovoto)
+		Dave Murphy (WinterMute)
+
+	This software is provided 'as-is', without any express or implied
+	warranty.  In no event will the authors be held liable for any
+	damages arising from the use of this software.
+
+	Permission is granted to anyone to use this software for any
+	purpose, including commercial applications, and to alter it and
+	redistribute it freely, subject to the following restrictions:
+
+	1.	The origin of this software must not be misrepresented; you
+		must not claim that you wrote the original software. If you use
+		this software in a product, an acknowledgment in the product
+		documentation would be appreciated but is not required.
+	2.	Altered source versions must be plainly marked as such, and
+		must not be misrepresented as being the original software.
+	3.	This notice may not be removed or altered from any source
+		distribution.
+
+---------------------------------------------------------------------------------*/
 #include <nds.h>
-#include <stdlib.h>
 
 //---------------------------------------------------------------------------------
 void startSound(int sampleRate, const void* data, u32 bytes, u8 channel, u8 vol,  u8 pan, u8 format) {
@@ -9,7 +35,6 @@ void startSound(int sampleRate, const void* data, u32 bytes, u8 channel, u8 vol,
 	SCHANNEL_LENGTH(channel) = bytes >> 2 ;
 	SCHANNEL_CR(channel)     = SCHANNEL_ENABLE | SOUND_ONE_SHOT | SOUND_VOL(vol) | SOUND_PAN(pan) | (format==1?SOUND_8BIT:SOUND_16BIT);
 }
-
 
 //---------------------------------------------------------------------------------
 s32 getFreeSoundChannel() {
@@ -21,7 +46,7 @@ s32 getFreeSoundChannel() {
 	return -1;
 }
 
-int vcount;
+
 touchPosition first,tempPos;
 
 //---------------------------------------------------------------------------------
@@ -54,29 +79,13 @@ void VcountHandler() {
 		but |= (1 <<6);
 	}
 
-	if ( vcount == 80 ) {
-		first = tempPos;
-	} else {
-		if (	abs( xpx - first.px) > 10 || abs( ypx - first.py) > 10 ||
-				(but & ( 1<<6)) ) {
-
-			but |= (1 <<6);
-			lastbut = but;
-
-		} else { 	
-			IPC->mailBusy = 1;
-			IPC->touchX			= x;
-			IPC->touchY			= y;
-			IPC->touchXpx		= xpx;
-			IPC->touchYpx		= ypx;
-			IPC->touchZ1		= z1;
-			IPC->touchZ2		= z2;
-			IPC->mailBusy = 0;
-		}
-	}
+	IPC->touchX			= x;
+	IPC->touchY			= y;
+	IPC->touchXpx		= xpx;
+	IPC->touchYpx		= ypx;
+	IPC->touchZ1		= z1;
+	IPC->touchZ2		= z2;
 	IPC->buttons		= but;
-	vcount ^= (80 ^ 130);
-	SetYtrigger(vcount);
 
 }
 
@@ -85,7 +94,6 @@ void VblankHandler(void) {
 //---------------------------------------------------------------------------------
 
 	u32 i;
-
 
 	//sound code  :)
 	TransferSound *snd = IPC->soundData;
@@ -101,29 +109,33 @@ void VblankHandler(void) {
 			}
 		}
 	}
-
 }
+
 
 //---------------------------------------------------------------------------------
 int main(int argc, char ** argv) {
 //---------------------------------------------------------------------------------
 
-	// Reset the clock if needed
-	rtcReset();
+	// read User Settings from firmware
+	readUserSettings();
 
 	//enable sound
 	powerON(POWER_SOUND);
+	writePowerManagement(PM_CONTROL_REG, ( readPowerManagement(PM_CONTROL_REG) & ~PM_SOUND_MUTE ) | PM_SOUND_AMP );
 	SOUND_CR = SOUND_ENABLE | SOUND_VOL(0x7F);
-	IPC->soundData = 0;
 
 	irqInit();
-	irqSet(IRQ_VBLANK, VblankHandler);
-	SetYtrigger(80);
-	vcount = 80;
-	irqSet(IRQ_VCOUNT, VcountHandler);
-	irqEnable(IRQ_VBLANK | IRQ_VCOUNT);
 
-	// Keep the ARM7 idle
+	// Start the RTC tracking IRQ
+	initClockIRQ();
+
+	SetYtrigger(80);
+	irqSet(IRQ_VCOUNT, VcountHandler);
+	irqSet(IRQ_VBLANK, VblankHandler);
+
+	irqEnable( IRQ_VBLANK | IRQ_VCOUNT);
+
+	// Keep the ARM7 mostly idle
 	while (1) swiWaitForVBlank();
 }
 
