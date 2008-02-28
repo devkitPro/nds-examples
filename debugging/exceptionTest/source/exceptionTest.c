22ee5d4988d955397746a8fd0ce96e2261ccf77e
Dave Murphy <wntrmute@users.sourceforge.net>
2008-02-28 13:40:04 +0000

    *** empty log message ***

--- debugging/exceptionTest/source/exceptionTest.c
+++ debugging/exceptionTest/source/exceptionTest.c
@@ -1,10 +1,17 @@
 #include <nds.h>
 
-// The default exception handler displays the exception type - data abort or undefined instruction
-// you can relate the exception to your code using arm-eabi-addr2line -e <elf file> <address>
-// assuming you built with debug info this will display a source file and a line number
-// The address of the instruction is shown as pc, beside the address which faulted
-// the rest of the screen is a dump of the registers.
+/*---------------------------------------------------------------------------------
+
+ The default exception handler displays the exception type - data abort or undefined
+ instruction you can relate the exception to your code using
+
+	arm-eabi-addr2line -e <elf file> <address>
+
+ assuming you built with debug info this will display a source file and a line number
+ The address of the instruction is shown as pc, beside the address which faulted
+ the rest of the screen is a dump of the registers.
+
+---------------------------------------------------------------------------------*/
 
 //---------------------------------------------------------------------------------
 int main(void) {
