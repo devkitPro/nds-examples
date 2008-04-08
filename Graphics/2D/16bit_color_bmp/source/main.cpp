d355167765858a3873e1dd39ef2aefb3333bafac
Jason Rogers <dovoto@users.sourceforge.net>
2008-04-08 00:50:03 +0000

    Updated for new background code and new decompress stuff

--- Graphics/2D/16bit_color_bmp/source/main.cpp
+++ Graphics/2D/16bit_color_bmp/source/main.cpp
@@ -1,6 +1,6 @@
 #include <nds.h>
 #include <stdio.h>
-#include <nds/decompress.h>
+
 
 // git outputs a nice header to reference data
 #include "drunkenlogo.h"
@@ -11,48 +11,28 @@ int main(void) {
 	irqEnable(IRQ_VBLANK);
 
     // set the mode for 2 text layers and two extended background layers
-	videoSetMode(MODE_5_2D | DISPLAY_BG3_ACTIVE);
+	videoSetMode(MODE_5_2D);
 
 	// set the sub background up for text display (we could just print to one
 	// of the main display text backgrounds just as easily
-	videoSetModeSub(MODE_0_2D | DISPLAY_BG0_ACTIVE); //sub bg 0 will be used to print text
+	videoSetModeSub(MODE_0_2D); //sub bg 0 will be used to print text
 
-    // set the first bank as background memory and the third as sub background memory
-    // B and D are not used (if you want a bitmap greater than 256x256 you will need more
-    // memory so another vram bank must be used and mapped consecutivly
-    vramSetMainBanks(	VRAM_A_MAIN_BG_0x06000000, VRAM_B_LCD,
-						VRAM_C_SUB_BG , VRAM_D_LCD);
+   vramSetBankA(VRAM_A_MAIN_BG);
 
-	// set up text background for text
-    SUB_BG0_CR = BG_MAP_BASE(31);
-
-	BG_PALETTE_SUB[255] = RGB15(31,31,31);//by default font will be rendered with color 255
-
-	//consoleInit() is a lot more flexible but this gets you up and running quick
-	consoleInitDefault((u16*)SCREEN_BASE_BLOCK_SUB(31), (u16*)CHAR_BASE_BLOCK_SUB(0), 16);
+	consoleDemoInit();
 
 	iprintf("\n\n\tHello DS devers\n");
 	iprintf("\twww.drunkencoders.com\n");
 	iprintf("\t16 bit bitmap demo");
 
 	// set up our bitmap background
-
-	BG3_CR = BG_BMP16_256x256;
-
-	// these are rotation backgrounds so you must set the rotation attributes:
-    // these are fixed point numbers with the low 8 bits the fractional part
-    // this basicaly gives it a 1:1 translation in x and y so you get a nice flat bitmap
-	BG3_XDX = 1 << 8;
-	BG3_XDY = 0;
-	BG3_YDX = 0;
-	BG3_YDY = 1 << 8;
-
-	BG3_CX = 0;
-	BG3_CY = 0;
-
-	decompress(BG_GFX, drunkenlogoBitmap, LZ77Vram);
+   int id = bgInit(3, BgType_Bmp16, BgSize_B16_256x256, 0,0);
 	
-	while(1) swiWaitForVBlank();
-
+	decompress(drunkenlogoBitmap, BG_GFX,  LZ77Vram);
+	
+	while(1)
+   {
+      swiWaitForVBlank();
+   }
 	return 0;
 }
