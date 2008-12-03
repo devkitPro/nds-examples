823cd7463b69ad0021ed97908b3fea1773d6d484
Jason Rogers <dovoto@users.sourceforge.net>
2008-12-03 21:07:12 +0000

    Updated for new api and removed a stray "while(1);"

--- Graphics/2D/Double_Buffer/source/main.cpp
+++ Graphics/2D/Double_Buffer/source/main.cpp
@@ -13,65 +13,52 @@
 int main(void) {
 //---------------------------------------------------------------------------------
 	//set the mode for 2 text layers and two extended background layers
-	videoSetMode(MODE_5_2D | DISPLAY_BG3_ACTIVE); 
-	
+	videoSetMode(MODE_5_2D); 
+
 	//set the first two banks as background memory and the third as sub background memory
 	//D is not used..if you need a bigger background then you will need to map
 	//more vram banks consecutivly (VRAM A-D are all 0x20000 bytes in size)
 	vramSetMainBanks(	VRAM_A_MAIN_BG_0x06000000, VRAM_B_MAIN_BG_0x06020000, 
-						VRAM_C_SUB_BG , VRAM_D_LCD); 
+		VRAM_C_SUB_BG , VRAM_D_LCD); 
 
 	consoleDemoInit();
 
 	iprintf("\n\n\tHello DS devers\n");
 	iprintf("\twww.drunkencoders.com\n");
 	iprintf("\tdouble buffer demo");
-	
-	while(1);
 
-	// set up our bitmap background
-	
-	REG_BG3CNT = BG_BMP16_256x256;
-	
-	// these are rotation backgrounds so you must set the rotation attributes:
-    // these are fixed point numbers with the low 8 bits the fractional part
-    // this basicaly gives it a 1:1 translation in x and y so you get a nice flat bitmap
-    REG_BG3PA = 1 << 8;
-    REG_BG3PB = 0;
-    REG_BG3PC = 0;
-    REG_BG3PD = 1 << 8;
 
-    // our bitmap looks a bit better if we center it so scroll down (256 - 192) / 2
-    REG_BG3X = 0;
-    REG_BG3Y = 32 << 8;
+	int bg = bgInit(3, BgType_Bmp16, BgSize_B16_256x256, 0,0);
 
-	
-	u16* frontBuffer = (u16*)(0x06000000);
-	u16* backBuffer =  (u16*)(0x06000000 + 256 * 256 * 2);
-	
-	//this is just used so we can write red color bits to one frame and green to the 
-	//other
 	u16 colorMask = 0x1F;
-	
-		while(1) {
-			//draw a box
-			for(int iy = 60; iy < 196 - 60; iy++)
-				for(int ix = 60; ix < 256 - 60; ix++) 
-					backBuffer[iy * 256 + ix] = (rand() & colorMask) | BIT(15);
-								
-			swiWaitForVBlank();
-				
-			//swap
-			u16* temp = frontBuffer;
-			frontBuffer = backBuffer;
-			backBuffer = temp;
-				
-			//flip 
-			//base is 16KB and screen size is 256x256x2 (128KB)
-			REG_BG3CNT ^= BG_BMP_BASE( 128 / 16 );
-				
-			//this will cause red or green bits only to be set and swap each
-			//frame
-			colorMask ^= 0x3FF;
+
+	u16* backBuffer = (u16*)bgGetGfxPtr(bg) + 256*256;
+
+	while(1) 
+	{
+		//draw a box (60,60,196,136)
+		for(int iy = 60; iy < 196 - 60; iy++)
+			for(int ix = 60; ix < 256 - 60; ix++) 
+				backBuffer[iy * 256 + ix] = (rand() & colorMask) | BIT(15);
+
+		swiWaitForVBlank();
+
+		//swap the back buffer to the current buffer
+		backBuffer = (u16*)bgGetGfxPtr(bg);
+
+		//swap the current buffer by changing the base. Each base
+		//represents 16KB of offset and each screen is 256x256x2 (128KB)
+		//this requires a map base seperation of 8 (could get away with smaller
+		//as the screen is really only showing 256x192 (96KB or map base 6)
+		if(bgGetMapBase(bg) == 8)
+		{
+			bgSetMapBase(bg, 0);
 		}
+		else
+		{
+			bgSetMapBase(bg, 8);
+		}
+
+		colorMask ^= 0x3FF;
+	}
 }
