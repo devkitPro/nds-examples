c13ecf40fef1160f1c0f1072ee92252b41fc56dd
Dave Murphy <wntrmute@users.sourceforge.net>
2008-12-05 05:02:39 +0000

    add basic maxmod example

---
+++ audio/maxmod/basicsound/source/MaxModExample.c
@@ -0,0 +1,77 @@
+#include <nds.h>
+#include <maxmod9.h>
+#include <stdio.h>
+
+#include "soundbank.h"
+#include "soundbank_bin.h"
+
+int main() {
+	
+	consoleDemoInit();
+
+	mmInitDefaultMem((mm_addr)soundbank_bin);
+	
+	// load the module
+	mmLoad( MOD_FLATOUTLIES );
+	
+	// load sound effects
+	mmLoadEffect( SFX_AMBULANCE );
+	mmLoadEffect( SFX_BOOM );
+
+	// Start playing module
+	mmStart( MOD_FLATOUTLIES, MM_PLAY_LOOP );
+
+	mm_sound_effect ambulance = {
+		{ SFX_AMBULANCE } ,			// id
+		(int)(1.0f * (1<<10)),	// rate
+		0,		// handle
+		255,	// volume
+		0,		// panning
+	};
+
+	mm_sound_effect boom = {
+		{ SFX_BOOM } ,			// id
+		(int)(1.0f * (1<<10)),	// rate
+		0,		// handle
+		255,	// volume
+		255,	// panning
+	};
+
+	// ansi escape sequence to clear screen and home cursor
+	// /x1b[line;columnH
+	iprintf("\x1b[2J");
+
+	// ansi escape sequence to set print co-ordinates
+	// /x1b[line;columnH
+	iprintf("\x1b[0;8HMaxMod Audio demo");
+	iprintf("\x1b[3;0HHold A for ambulance sound");
+	iprintf("\x1b[4;0HPress B for boom sound");
+
+	do {
+
+		int keys_pressed, keys_released;
+		mm_sfxhand amb = 0;
+
+		swiWaitForVBlank();
+		scanKeys();
+
+		keys_pressed = keysDown();
+		keys_released = keysUp();
+
+		// Play looping ambulance sound effect out of left speaker if A button is pressed
+		if ( keys_pressed & KEY_A ) {
+			amb = mmEffectEx(&ambulance);
+		}
+
+		// stop ambulance sound when A button is released
+		if ( keys_released & KEY_A ) {
+			mmEffectCancel(amb);
+		}
+
+		// Play explosion sound effect out of right speaker if B button is pressed
+		if ( keys_pressed & KEY_B ) {
+			mmEffectEx(&boom);
+		}
+
+	} while( 1 );
+}
