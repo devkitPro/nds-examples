fbadc46932a7f33e5f5a0645e8a88485f6d6698b
Jason Rogers <dovoto@users.sourceforge.net>
2008-12-08 17:36:14 +0000

    Initial add of event sample

---
+++ audio/maxmod/song_events_example/source/template.c
@@ -0,0 +1,109 @@
+/*		Simple Maxmod example - Song events   
+
+	See http://www.maxmod.org/ref/tut/songevents.html for an explanation of song events.
+
+*/
+
+
+
+
+#include <nds.h>
+
+#include <maxmod9.h>
+#include "mmsolution.h"		// solution definitions
+#include "mmsolution_bin.h"	// solution binary reference 
+#include "ball.h"
+
+int spriteDy = 0;
+int spriteY = 140;
+
+// callback function to handle song events
+mm_word myEventHandler( mm_word msg, mm_word param )
+{
+    switch( msg )
+    {
+    case MMCB_SONGMESSAGE:	// process song messages
+
+			// if song event 1 is triggered, set sprite's y velocity to make it jump
+			if (param == 1) spriteDy = -16;
+		
+        break;
+		
+    case MMCB_SONGFINISHED:	// process song finish message (only triggered in songs played with MM_PLAY_ONCE)
+        
+		break;
+    }
+	
+	return 0;
+}
+
+
+//---------------------------------------------------------------------------------
+int main(void) {
+//---------------------------------------------------------------------------------
+	
+	videoSetMode(MODE_0_2D);
+	
+	//initialize the sprite engine with 1D mapping 128 byte boundary
+	//and no external palette support
+	oamInit(&oamMain, SpriteMapping_1D_32, false);
+
+	vramSetBankA(VRAM_A_MAIN_SPRITE);
+
+	//allocate some space for the sprite graphics	
+	u16* gfx = oamAllocateGfx(&oamMain, SpriteSize_32x32, SpriteColorFormat_256Color);
+
+	//fill the 256 color sprite with index 1 (2 pixels at a time)
+	dmaCopy(ballTiles, gfx, ballTilesLen);
+	dmaCopy(ballPal, SPRITE_PALETTE, ballPalLen);
+	
+	//--------------------------------------------
+	// initialise maxmod using default settings, and enable interface for soundbank that is loaded into memory
+	//--------------------------------------------
+	mmInitDefaultMem( (mm_addr)mmsolution_bin );
+
+	// setup maxmod to use the song event handler
+	mmSetEventHandler( myEventHandler );
+	
+	// load song
+	// values for this function are in the solution header
+	mmLoad( MOD_EXAMPLE );
+	// start the music playing
+	mmStart( MOD_EXAMPLE, MM_PLAY_LOOP );
+
+	while(1) 
+	{
+		// Sprite accelerates down
+		spriteDy += 2;
+		
+		// sprite falls
+		spriteY += spriteDy;
+		
+		// Floor is arbitrarily set to 140
+		if ( spriteY > 140 ) 
+			spriteY = 140;
+		
+		
+		oamSet(&oamMain, 			//main graphics engine context
+			0,           			//oam index (0 to 127)  
+			256/2-16,				//x and y pixel location of the sprite
+			spriteY, 			
+			0,						//priority, lower renders last (on top)
+			0,	//palette index if multiple palettes or the alpha value if bmp sprite	
+			SpriteSize_32x32,
+			SpriteColorFormat_256Color,
+			gfx,				//pointer to the loaded graphics
+			-1,	//sprite rotation data  
+			false,					//double the size when rotating?
+			false);					//hide the sprite?
+
+		swiWaitForVBlank();
+		
+		//send the updates to the hardware
+		oamUpdate(&oamMain);
+	}
+
+	return 0;
+}
+
+
