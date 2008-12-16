22962d7dff01f3c09b1c43954c36a61965c0be41
Jason Rogers <dovoto@users.sourceforge.net>
2008-12-16 19:13:28 +0000

    Added an async keyboard example, moved some more examples around

---
+++ input/keyboard/keyboard_async/source/template.c
@@ -0,0 +1,30 @@
+/*---------------------------------------------------------------------------------
+
+	Simple console print demo
+	-- dovoto
+
+---------------------------------------------------------------------------------*/
+#include <nds.h>
+#include <stdio.h>
+
+//---------------------------------------------------------------------------------
+int main(void) {
+//---------------------------------------------------------------------------------
+	consoleDemoInit();  //setup the sub screen for printing
+
+	keyboardInit(keyboardGetDefault());
+
+	keyboardShow();
+
+	while(1) {
+		
+		int key = keyboardUpdate();
+
+		if(key > 0)
+			iprintf("%c", key);
+
+		swiWaitForVBlank();
+	}
+
+	return 0;
+}
