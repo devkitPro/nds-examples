1b185a87ae58bf3e6198ca6e6d20f47930a9df92
vuurrobin <vuurrobin@users.sourceforge.net>
2010-07-17 15:00:58 +0000

    added an stopwatch and timercallback example and removed CR as line ending.

---
+++ time/stopwatch/source/main.c
@@ -0,0 +1,73 @@
+#include <nds.h>
+
+#include <stdio.h>
+#include <time.h>
+
+//the speed of the timer when using ClockDivider_1024
+#define TIMER_SPEED (TIMER_HZ/1024)
+
+typedef enum
+{
+	timerState_Stop,
+	timerState_Pause,
+	timerState_Running
+}TimerStates;
+
+
+int main()
+{
+	consoleDemoInit();
+
+	uint ticks = 0;
+	TimerStates state = timerState_Stop;
+	int down = keysDown();
+
+	while(!(down & KEY_START))
+	{
+		swiWaitForVBlank();
+		consoleClear();
+		scanKeys();
+		down = keysDown();
+
+		if(state == timerState_Running)
+		{
+			ticks += timerElapsed(0);
+		}
+
+		if(down & KEY_A)
+		{
+			if(state == timerState_Stop)
+			{
+				timerStart(0, ClockDivider_1024, 0, NULL);
+				state = timerState_Running;
+			}
+			else if(state == timerState_Pause)
+			{
+				timerUnpause(0);
+				state = timerState_Running;
+			}
+			else if(state == timerState_Running)
+			{
+				ticks += timerPause(0);
+				state = timerState_Pause;
+			}
+		}
+		else if(down & KEY_B)
+		{
+			timerStop(0);
+			ticks = 0;
+			state = timerState_Stop;
+		}
+
+		iprintf("Press A to start and pause the \ntimer, B to clear the timer \nand start to quit the program.\n\n");
+		iprintf("ticks:  %u\n", ticks);
+		iprintf("second: %u:%u\n", ticks/TIMER_SPEED, ((ticks%TIMER_SPEED)*1000) /TIMER_SPEED);
+	}
+
+	if(state != timerState_Stop)
+	{
+		timerStop(0);
+	}
+
+	return 0;
+}
---
+++ time/timercallback/source/main.c
@@ -0,0 +1,39 @@
+#include <nds.h>
+
+void waitfor(int keys)
+{
+	scanKeys();
+	while((keysDown() & keys) == 0)
+	{
+		swiWaitForVBlank();
+		scanKeys();
+	}
+}
+
+
+uint channel = 0;
+bool play = true;
+
+//this function will be called by the timer.
+void timerCallBack()
+{
+	if(play)
+		soundPause(channel);
+	else
+		soundResume(channel);
+
+	play = !play;
+}
+
+int main()
+{
+	soundEnable();
+	channel = soundPlayPSG(DutyCycle_50, 10000, 127, 64);
+
+	//calls the timerCallBack function 5 times per second.
+	timerStart(0, ClockDivider_1024, TIMER_FREQ_1024(5), timerCallBack);
+
+	waitfor(KEY_A);
+
+	return 0;
+}
