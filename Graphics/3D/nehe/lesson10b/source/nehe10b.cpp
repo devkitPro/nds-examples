e48dbe100231b693d79cb44d6493ffc974240572
Dave Murphy <wntrmute@users.sourceforge.net>
2010-11-23 20:06:21 +0000

    fix texture storage

--- Graphics/3D/nehe/lesson10b/source/nehe10b.cpp
+++ Graphics/3D/nehe/lesson10b/source/nehe10b.cpp
@@ -3,15 +3,11 @@
  * 		Author: Dovoto					*
  ****************************************/
 
-// include your ndslib
+// include your libnds
 #include <nds.h>
+
 #include <malloc.h>
 #include <stdio.h>
-
-//needed to load pcx files
-#include <nds/arm9/image.h>
-#include <nds/arm9/trig_lut.h>
-
 
 #include "Mud_pcx.h"
 #include "World_txt.h"
@@ -29,37 +25,31 @@ int walkbiasangle = 0;
 int lookupdown = 0;
 
 
-int	texture[0];			// Storage For 1 Textures (only going to use 1 on the DS for this demo)
+int	texture[1];			// Storage For 1 Textures (only going to use 1 on the DS for this demo)
 
-typedef struct tagVERTEX
-{
+typedef struct tagVERTEX {
 	v16 x, y, z;
 	t16 u, v;
 } VERTEX;
 
-typedef struct tagTRIANGLE
-{
+typedef struct tagTRIANGLE {
 	VERTEX vertex[3];
 } TRIANGLE;
 
-typedef struct tagSECTOR
-{
+typedef struct tagSECTOR {
 	int numtriangles;
 	TRIANGLE* triangle;
 } SECTOR;
 
-  SECTOR sector1;				// Our Model Goes Here:
+SECTOR sector1;				// Our Model Goes Here:
 
+u8* file = (u8*)World_txt;
 
+void myGetStr(char* buff, int size) {
 
- u8* file = (u8*)World_txt;
-
- void myGetStr(char* buff, int size)
-{
 	*buff = *file++;
 
-	while( (*buff != '\n') && (*buff != 0xD))
-	{
+	while( (*buff != '\n') && (*buff != 0xD)) {
 		buff++;
 		*buff = *file++;
 	}
@@ -70,17 +60,15 @@ typedef struct tagSECTOR
 }
 
 
-void readstr(char *string)
-{
-	do
-	{
+void readstr(char *string) {
+
+	do {
 		myGetStr(string, 255);
 	} while ((string[0] == '/') || (string[0] == '\n' ));
-	return;
+
 }
 
-void SetupWorld()
-{
+void SetupWorld() {
 	float x, y, z; 
 	float u, v;
 	int numtriangles;
@@ -92,10 +80,8 @@ void SetupWorld()
 	sector1.triangle = (TRIANGLE*)malloc(numtriangles*sizeof(TRIANGLE));
 	sector1.numtriangles = numtriangles;
 	
-	for (int loop = 0; loop < numtriangles; loop++)
-	{
-		for (int vert = 0; vert < 3; vert++)
-		{
+	for (int loop = 0; loop < numtriangles; loop++) {
+		for (int vert = 0; vert < 3; vert++) {
 			readstr(oneline);
 			sscanf(oneline, "%f %f %f %f %f", &x, &y, &z, &u, &v);
 			sector1.triangle[loop].vertex[vert].x = floattov16(x);
@@ -108,9 +94,8 @@ void SetupWorld()
 	
 	return;
 }
-int LoadGLTextures()									// Load PCX files And Convert To Textures
-{
-	sImage pcx;                //////////////(NEW) and different from nehe.
+int LoadGLTextures() {			// Load PCX files And Convert To Textures
+	sImage pcx;					//(NEW) and different from nehe.
 
 	//load our texture
 	loadPCX((u8*)Mud_pcx, &pcx);
@@ -173,33 +158,31 @@ int main() {
 	// Set the current matrix to be the model matrix
 	glMatrixMode(GL_MODELVIEW);
 	
-	while (1) 
-	{
+	while (1) {
 		//these little button functions are pretty handy
 		scanKeys();
 				
-	
-		if (keysHeld() & KEY_A)
-		{
+		int held = keysHeld();
+		
+		if (held & KEY_A) {
 			lookupdown -= 1;
 		}
-		if (keysHeld() & KEY_B)
-		{
+
+		if (held & KEY_B) {
 			lookupdown += 1;
 		}
 		
-		if (keysHeld() & KEY_LEFT)
-		{
+		if (held & KEY_LEFT) {
 			heading += 64;	
 			yrot = heading;
 		}
-		if (keysHeld() & KEY_RIGHT)
-		{
+
+		if (held & KEY_RIGHT) {
 			heading -= 64;
 			yrot = heading;
 		}
-		if (keysHeld() & KEY_DOWN)
-		{
+
+		if (held & KEY_DOWN) {
 			
 			xpos += sinLerp(heading)/20;
 			zpos += cosLerp(heading)/20;
@@ -208,8 +191,8 @@ int main() {
 			
 			walkbias = sinLerp(walkbiasangle)/20;
 		}
-		if (keysHeld() & KEY_UP)
-		{
+
+		if (held & KEY_UP) {
 			xpos -= sinLerp(heading)/20;
 			zpos -= cosLerp(heading)/20;
 
@@ -230,15 +213,15 @@ int main() {
 	return 0;
 }
 
-int DrawGLScene()											// Here's Where We Do All The Drawing
-{
-											// Reset The View
+int DrawGLScene() {							// Here's Where We Do All The Drawing
+
+	// Reset The View
 
 	v16 x_m, y_m, z_m;
 	t16 u_m, v_m;
-	int32 xtrans = -xpos;
-	int32 ztrans = -zpos;
-	int32 ytrans = -walkbias-(1<<10);
+	s32 xtrans = -xpos;
+	s32 ztrans = -zpos;
+	s32 ytrans = -walkbias-(1<<10);
 	int sceneroty = LUT_SIZE - yrot;
 	
 	glLoadIdentity();
@@ -255,8 +238,8 @@ int DrawGLScene()											// Here's Where We Do All The Drawing
 	
 	
 	// Process Each Triangle
-	for (int loop_m = 0; loop_m < numtriangles; loop_m++)
-	{
+	for (int loop_m = 0; loop_m < numtriangles; loop_m++) {
+
 		glBegin(GL_TRIANGLES);
 			glNormal(NORMAL_PACK( 0, 0, 1<<10));
 			x_m = sector1.triangle[loop_m].vertex[0].x;
