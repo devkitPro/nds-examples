a953f44cd960ba2d5aa874c9bf3efbdc5c3c7719
Dave Murphy <wntrmute@users.sourceforge.net>
2011-02-01 05:13:34 +0000

    build arm7 as C, update for libnds changes

--- userfifo/arm7/source/arm7.c
+++ userfifo/arm7/source/arm7.c
@@ -48,9 +48,11 @@ void VcountHandler() {
 volatile bool exitflag = false;
 
 //---------------------------------------------------------------------------------
-void powerButtonHandler() {
+void i2cIRQHandler() {
 //---------------------------------------------------------------------------------
-	exitflag = true;
+	int cause = (i2cReadRegister(I2C_PM, 0x10) & 0x3) | (i2cReadRegister(I2C_UNK4, 0x02)<<2);
+	
+	if (cause & 1) exitflag = true;
 }
 
 //---------------------------------------------------------------------------------
@@ -74,8 +76,8 @@ int main() {
 
 	irqSet(IRQ_VCOUNT, VcountHandler);
 	irqSet(IRQ_VBLANK, VblankHandler);
-	irqSetAUX(IRQ_POWER, powerButtonHandler);
-	irqEnableAUX(IRQ_POWER);
+	irqSetAUX(IRQ_I2C, i2cIRQHandler);
+	irqEnableAUX(IRQ_I2C);
 
 	irqEnable( IRQ_VBLANK | IRQ_VCOUNT | IRQ_NETWORK);   
 
