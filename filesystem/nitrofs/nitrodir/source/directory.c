0d91ab18cae52a87096561aacecb6acd5ff30516
zeromus <zeromus@users.sourceforge.net>
2011-02-03 06:21:46 +0000

    make the nitrofs test more testful

--- filesystem/nitrofs/nitrodir/source/directory.c
+++ filesystem/nitrofs/nitrodir/source/directory.c
@@ -55,6 +55,35 @@ int main(int argc, char **argv) {
 	if (nitroFSInit()) {
 
 		dirlist("/");
+		
+		{
+			// now, try reading a file to make sure things are working OK.
+			FILE* inf = fopen("file1.txt","rb");
+			if(inf)
+			{
+				int len;
+
+				fseek(inf,0,SEEK_END);
+				len = ftell(inf);
+				fseek(inf,0,SEEK_SET);
+
+				iprintf("\nthe following %d bytes message\nfrom file1.txt is\nbrought to you by fread:\n",len);
+				{
+					char *entireFile = (char*)malloc(len+1);
+					entireFile[len] = 0;
+					if(fread(entireFile,1,len,inf) != len)
+						iprintf("savage error reading the bytes from the file!\n");
+					else
+						iprintf("%s\n-done-\n",entireFile);
+					free(entireFile);
+				}
+
+				fclose(inf);
+			}
+		}
+
+		iprintf("here is the dirlist once more:\n");
+		dirlist("/");
 
 	} else {
 		iprintf("nitroFSInit failure: terminating\n");
