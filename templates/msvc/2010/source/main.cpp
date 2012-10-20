37550c45dc3518b6fb880bd5da639c0389c65f5b
zeromus <zeromus@zeromus.org>
2012-10-20 17:24:50 -0500

    install mtheall's mother of all templates and demo some of the stuff. still need to demo grit and obj2bin files. we should probably separate the template from the example-of-template because im not sure im happy with so much cruft being in the template

--- templates/msvc/2010/source/main.cpp
+++ templates/msvc/2010/source/main.cpp
@@ -1,15 +1,9 @@
-/*---------------------------------------------------------------------------------
-
-	$Id: main.cpp,v 1.13 2008-12-02 20:21:20 dovoto Exp $
-
-	Simple console print demo
-	-- dovoto
-
-
----------------------------------------------------------------------------------*/
 #include <nds.h>
-
+#include <maxmod9.h>
+#include <filesystem.h>
 #include <stdio.h>
+
+#include "soundbank.h"
 
 volatile int frame = 0;
 
@@ -28,10 +22,37 @@ int main(void) {
 
 	consoleDemoInit();
 
-	iprintf("      Hello DS dev'rs\n");
-	iprintf("     \x1b[32mwww.devkitpro.org\n");
-	iprintf("   \x1b[32;1mwww.drunkencoders.com\x1b[39m");
- 
+	nitroFSInit(NULL);
+
+	//define a sound effect's noteon parameters
+	mm_sound_effect badong = {
+		{ SFX_BADONG } ,			// id
+		(int)(1.0f * (1<<10)),	// rate
+		0,		// handle
+		255,	// volume
+		255,	// panning
+	};
+
+	//initialize maxmod; provide path to soundbank.bin. nitroFS will be used to open the file
+	mmInitDefault("soundbank.bin");
+
+	//load and play a sound effect
+	mmLoadEffect( SFX_BADONG );
+	mmEffectEx(&badong);
+
+	//load and play a mod
+	mmLoad( MOD_MOD );
+	mmStart( MOD_MOD, MM_PLAY_LOOP );
+
+	//load a script and print it to screen
+	FILE* inf = fopen("script.txt","rb");
+	static char buf[128];
+	while(fgets(buf,128,inf))
+	{
+		iprintf("%s",buf);
+	}
+	fclose(inf);
+
 	while(1) {
 	
 		swiWaitForVBlank();
@@ -41,7 +62,6 @@ int main(void) {
 		iprintf("\x1b[10;0HFrame = %d",frame);
 		iprintf("\x1b[16;0HTouch x = %04X, %04X\n", touchXY.rawx, touchXY.px);
 		iprintf("Touch y = %04X, %04X\n", touchXY.rawy, touchXY.py);		
-	
 	}
 
 	return 0;
